37876325b7756a61f22aed8eba4f288096461689
danil.malkin <danil.malkin@stm-labs.ru>
2022-11-21 19:43:40 +0300

    work on recycler view

--- app/src/main/java/io/develop/restaurants/FirstFragment.kt
+++ app/src/main/java/io/develop/restaurants/FirstFragment.kt
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import io.develop.restaurants.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -19,12 +20,16 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+
+
         return binding.root
 
     }
@@ -32,9 +37,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 
     override fun onDestroyView() {
